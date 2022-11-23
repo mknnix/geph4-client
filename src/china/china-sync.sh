f86c4423896a3333ff4f0e93238b07beaedf1c35
Your Name <you@example.com>
2022-11-23 19:28:10 +0800

    src/china/: add converter from txt to json

--- src/china/china-sync.sh
+++ src/china/china-sync.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
+txt2json(){
+	if test -z "$1"
+	then
+		echo "Internal Error: missing args"
+		exit 1
+	fi
+
+	cat "$1" | awk 'BEGIN{printf "["} {printf "\""$0"\","} END {print "\"\"]"}'
+}
+
 racket generate-china.sh > china-domains.txt
+txt2json china-domains.txt > china-domains.json
+
 curl https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt > china-ips.txt
+txt2json china-ips.txt > china-ips.json
+
