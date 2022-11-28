a83de40cfe3e0cc83a4f1b8dc17332e3f2060051
Your Name <you@example.com>
2022-11-28 19:11:27 +0800

    src/logs.rs: added log rotating

--- src/logs.rs
+++ src/logs.rs
@@ -1,5 +1,55 @@
 use std::collections::VecDeque;
+use std::time::{Instant, Duration};
 
+#[derive(Clone)]
+pub struct LogRotate {
+    buf: LogBuffer,
+    timeout: Duration,
+    started: Instant,
+}
+impl LogRotate {
+    pub fn new(mem: usize, maxtime: Duration) -> Self {
+        Self{
+            buf: LogBuffer::new(mem),
+            timeout: Instant::now(),
+            started: run
+        }
+    }
+
+    pub fn add_line(&mut self, line: &str) -> bool {
+        let res: bool;
+
+        if self.started.elapsed(Instant::now()) > self.timeout {
+            self.buf.clear();
+            println!("[Geph4-cli] removed all logs from memory (due to timeout {:?})");
+
+            res = true;
+        } else {
+            res = false;
+        }
+
+        self.buf.add_line(line);
+        return res;
+    }
+
+    pub fn logs(&self) -> LogBuffer {
+        let mut snap = self.clone();
+        snap.add_line(
+            &format!(
+                "Geph logs dumped by time {:?}",
+                std::time::SystemTime::now()
+            )
+        );
+
+        snap.buf
+    }
+
+    pub fn get_logs(&self) -> String {
+        self.logs().get_logs()
+    }
+}
+
+#[derive(Clone)]
 pub struct LogBuffer {
     logs: VecDeque<char>,
     mem_limit: usize, // in # of characters
@@ -22,6 +72,15 @@ impl LogBuffer {
         while self.logs.len() > self.mem_limit {
             self.logs.pop_front();
         }
+    }
+
+    pub fn clear(&mut self) -> bool {
+        if self.logs.len() <= 0 {
+            return false;
+        }
+
+        self.logs = VecDeque::new();
+        return true;
     }
 
     pub fn get_logs(&self) -> String {
